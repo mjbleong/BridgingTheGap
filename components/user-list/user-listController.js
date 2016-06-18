33702826d7c2fa6a1c31993bcd92b9882d39c64f
Matthew Leong <mjbleong@stanford.edu>
2016-06-18 01:37:34 -0700

    added AngularFire

--- components/user-list/user-listController.js
+++ components/user-list/user-listController.js
@@ -1,8 +1,12 @@
 'use strict';
 
-cs142App.controller('UserListController', ['$scope', '$resource',
-    function ($scope, $resource) {
+cs142App.controller('UserListController', ['$scope', '$resource', '$firebaseObject',
+    function ($scope, $resource, $firebaseObject) {
         $scope.main.title = 'Users';
+        var ref = firebase.database().ref();
+        // download the data into a local object
+        var testObject = $firebaseObject(ref);
+        $scope.main.testObj = testObject;
 
 	    var UserList = $resource('/user/list', {
 	      query: {method: 'get', isArray: true}
