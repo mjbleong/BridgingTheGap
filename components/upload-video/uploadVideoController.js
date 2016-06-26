b6efed646fb2c8868ce1fb987ce336bcf120fd20
Marisa <marisapi@stanford.edu>
2016-06-26 00:32:44 -0700

    merged all three

--- components/upload-video/uploadVideoController.js
+++ components/upload-video/uploadVideoController.js
@@ -45,22 +45,22 @@ cs142App.controller('UploadVideoController', ['$scope', '$routeParams', '$resour
       $scope.lname = snapshot.val().lastname;
     });
 
-  	$scope.clipchamp = function(data) {
+    $scope.clipchamp = function(data) {
 
-  		var currDate = new Date();
+      var currDate = new Date();
 
-  		var yt_title = $scope.fname + ' ' + $scope.lname + ' ' + currDate;
+      var yt_title = $scope.fname + ' ' + $scope.lname + ' ' + currDate;
 
-  		var dbDate = '' + currDate;
+      var dbDate = '' + currDate;
 
-  		var process = clipchamp({
+      var process = clipchamp({
 
         resolution: "720p",
         preset: "web",
         title: "Click submit and you're done. Thanks!",
         output: "youtube",
         youtube: {
-        	title: yt_title,
+          title: yt_title,
           description: 'wut wut'
         },
 
@@ -71,29 +71,29 @@ cs142App.controller('UploadVideoController', ['$scope', '$routeParams', '$resour
 
           //function to extract id from youtube url
           function getId(url) {
-    		    var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-    		    var match = url.match(regExp);
+            var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+            var match = url.match(regExp);
 
-    		    if (match && match[2].length == 11) {
-    		        return match[2];
-    		    } else {
-    		        return 'error';
-    		    }
-		      };
+            if (match && match[2].length == 11) {
+                return match[2];
+            } else {
+                return 'error';
+            }
+          };
 
-      		var myId = getId(data.url);
+          var myId = getId(data.url);
 
-      		var dbDate = new Date();
+          var dbDate = new Date();
 
-      		var newPostKey = firebase.database().ref('users/' + $scope.currentUser + "/videos")
-      			.push('').key;
+          var newPostKey = firebase.database().ref('users/' + $scope.currentUser + "/videos")
+            .push('').key;
 
-      		firebase.database().ref('videos/' + newPostKey).set({
-      				author_id: $scope.currentUser,
-      				date: dbDate,
-      				title: "dfhadfjadp",
-      				url: myId
-      		});
+          firebase.database().ref('videos/' + newPostKey).set({
+              author_id: $scope.currentUser,
+              date: dbDate,
+              title: "dfhadfjadp",
+              url: myId
+          });
 
           $scope.upload.videoId = newPostKey;
 
@@ -106,7 +106,7 @@ cs142App.controller('UploadVideoController', ['$scope', '$routeParams', '$resour
         }      
       });
 
-	process.open();
-	}
+  process.open();
+  }
   }
   ]);
